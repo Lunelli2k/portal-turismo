95f54d9e1f91594621b7943276261eb6d172f929
Gabriel <gabriellunelli2k19@gmail.com>
2025-08-31 22:18:44 -0300

    PDT-43 e PDT-40: Criação do projeto inicial e o docker compose do banco de dados

---
+++ backend/src/main/java/br/com/portalturismo/backend/PortalTurismoBackendApplication.java
@@ -0,0 +1,15 @@
+package br.com.portalturismo.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalTurismoBackendApplication {
+
+	public static void main(String[] args) {
+        System.out.println("Hello World");
+        SpringApplication.run(PortalTurismoBackendApplication.class, args);
+
+	}
+
+}
---
+++ backend/src/test/java/br/com/portalturismo/backend/PortalTurismoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.portalturismo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalTurismoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
